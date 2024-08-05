fa3b917b64b18b91a02b33d584d6746ef7a9d66d
Subhash <subhashnaidu77>
2024-08-05 16:40:44 +0530

    first commit

--- src/pages/Desktop/VolumeSection.jsx
+++ src/pages/Desktop/VolumeSection.jsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function VolumeSection() {
 return (
 <>
-<div className="relativecontent-end">
+<div className="relativecontent-end  border box-border border-#e9e9e9 border-t-0  w-[66.9%] ">
 <div className="h-[260px] flex-1 self-center sm:h-auto"> 
-<div className="absolute bottom-e left-e right-e top-e m-auto h-max flex-1 mt-6">
+<div className="absolute bottom-e left-e right-e top-e m-auto h-max flex-1 ">
 <div className="flex items-start">
-    <Img src="images/img_line.svg" alt="Line Image" className="mb-[68px] h-[272px] w-[900px] mt-2  sm:h-auto" />
-    <div className="relative ml-[-40px] mt-[55px] flex w-[12%] flex-col gap-14 sm:gap-7">
+    <Img src="images/img_line.svg" alt="Line Image" className="mb-[68px] h-[290px] w-[900px] mt-2 ml-[-46px]   sm:h-auto" />
+    <div className="relative  mt-[55px] flex w-[10%] flex-col gap-14 sm:gap-7">
     
-<Button color="blue_gray_900" shape="round" className="self-stretch mt-[-17px] ml-[-35px] mr-6">
+<Button color="blue_gray_900" shape="round" className="self-stretch mt-[-12px] ml-[-70px] ">
 <span className="items-center justify-center mt-[4px]">
 
 64,850.35</span>
 </Button>
-<Button shape="round" className="mr-2.5 self-stretch mt-[12px] ml-[-30px] sm:mr-0 mr-4 ">
+<Button shape="round" className="mr-8 self-stretch mt-[12px] ml-[-55px] ">
 <span className="items-center justify-center mt-[5px]">
 
 63,179.71
@@ -30,9 +30,9 @@ return (
 </div>
 
 
-<Img src="images/img_volume.svg" alt="Volume Icon" className=" h-[32.2px] ml-[61.9px]   mt-12  " />
+<Img src="images/img_volume.svg" alt="Volume Icon" className=" h-[34px] ml-[2px]   mt-12  " />
 
-<Img src="images/img_line2.svg" alt="Volume Icon 2" className="mb-1 h-[11px] mt-[-5.9px] w-[82%]  max-w-[63%] ml-[65px] sm:ml-0 " />
+
 </div>
 
 
